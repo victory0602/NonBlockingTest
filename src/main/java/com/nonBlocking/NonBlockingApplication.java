ee24354032e8be04a112eefc10e39e45b3ef0f1c
seungri_Home <xodidqhf@gmail.com>
2022-01-24 23:55:51 +0900

    [master/dev] Project생성 1. Project생성

---
+++ src/main/java/com/nonBlocking/NonBlockingApplication.java
@@ -0,0 +1,13 @@
+package com.nonBlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonBlockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonBlockingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nonBlocking/NonBlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonBlocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonBlockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
