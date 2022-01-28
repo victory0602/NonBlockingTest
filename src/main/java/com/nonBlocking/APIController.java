2e2b3635a482b42fbe1c3e03e4ae5af61af57f59
seungri_Home <xodidqhf@gmail.com>
2022-01-29 02:56:43 +0900

    [02_NonBlocking-WebClientI/dev] 각 건별 NonBlocking API 호출 Test 작성 01. ResponseData객체의 Thread 레벨의 동기화로 인하여 Response Server에서 건별 데이터를 전달받지 못하였다. 이를 해결하고자 지역변수를 이용하여 테스트를 추가하였다. (04번)

--- src/main/java/com/nonBlocking/APIController.java
+++ src/main/java/com/nonBlocking/APIController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.util.HashMap;
 
@@ -23,6 +25,7 @@ public class APIController {
     ApplicationContext responseDataAC = new AnnotationConfigApplicationContext(ResponseData.class);
     ResponseData responseData = responseDataAC.getBean(ResponseData.class);
     String url = "http://localhost:8080/apiTest";
+    WebClient webClient = WebClient.create("http://localhost:8080");
 
     /*
     * Blocking 통신 구현을 위한 RestTemplate 선언
@@ -35,6 +38,7 @@ public class APIController {
     public ResponseEntity<ResponseData> Blocking() {
         log.trace("Call blocking.");
 
+        // 시간 측정을 위해 선언
         final StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
@@ -46,6 +50,7 @@ public class APIController {
         HttpEntity<ResponseData> responseDataHttpEntity = new HttpEntity<>(responseData, headers);
         ResponseEntity responseEntity = null;
         for(int i = 0; i < 3; i++) {
+            // Response Server API 호출
             responseEntity = restTemplate.exchange(url, HttpMethod.POST, responseDataHttpEntity, ResponseData.class);
             String body = responseEntity.getBody().toString();
             log.info("body : {}", body);
@@ -71,11 +76,11 @@ public class APIController {
         for(int i = 0; i < 3; i++) {
             responseEntityMono = webClient.post()
                     .uri("/apiTest")
-                    //.accept(MediaType.APPLICATION_JSON)
                     .bodyValue(responseData)
                     .retrieve()
                     .bodyToMono(ResponseData.class);
 
+            // block()메소드를 이용하여 결과 값을 가져오는데 여기서 Block이 걸린다.
             ResponseData body = responseEntityMono.block();
             log.info("body : {}", body.toString());
         }
@@ -94,18 +99,17 @@ public class APIController {
         final StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        responseData.setErrorMsg("http://localhost:18080/nonBlocking - Call nonBlocking webcline02.");
-
         Mono<ResponseData> responseEntityMono = null;
         for(int i = 0; i < 3; i++) {
+            responseData.setErrorMsg("http://localhost:18080/nonBlocking - Call nonBlocking webcline02(" + i + ")");
             webClient.post()
                     .uri("/apiTest")
-                    //.accept(MediaType.APPLICATION_JSON)
                     .bodyValue(responseData)
                     .retrieve()
                     .bodyToMono(ResponseData.class)
-            .subscribe(response ->
-                    log.info("body : {}", response));
+                    // 호출 순서를 확인하기 위한 응답 값 출력
+                    .subscribe(response ->
+                            log.info("body : {}", response));
         }
 
         stopWatch.stop();
@@ -122,36 +126,68 @@ public class APIController {
         final StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        responseData.setErrorMsg("http://localhost:18080/nonBlocking - Call nonBlocking webcline03.");
-
         Mono<ResponseEntity<ResponseData>> responseEntityMono = null;
         for(int i = 0; i < 3; i++) {
+            responseData.setErrorMsg("http://localhost:18080/nonBlocking - Call nonBlocking webcline03(" + i + ")");
+            log.info("responseData.getErrorMsg() : {}", responseData.getErrorMsg());
             responseEntityMono = webClient.post()
                     .uri("/apiTest")
-                    //.accept(MediaType.APPLICATION_JSON)
                     .bodyValue(responseData)
                     .retrieve()
                     .toEntity(ResponseData.class);
         }
 
-        ResponseEntity body = responseEntityMono.block();
-        log.info("body : {}", body.toString());
-
+        /*
+        * 비즈니스 로직 수행 ...
+        * */
+        responseEntityMono.subscribe(response ->
+                log.info("body : {}", response));
         stopWatch.stop();
         log.info("Total Second : {}", stopWatch.getTotalTimeSeconds());
 
         return Mono.fromSupplier(() -> ResponseEntity.status(HttpStatus.OK).body(responseData));
     }
 
+    @PostMapping("/nonBlocking/webcline04")
+    public Mono<ResponseEntity<ResponseData>> NonBlockingWebClient04() {
+        log.trace("Call nonBlocking webcline04.");
+
+
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Flux.range(0,3)
+                .flatMap(t ->
+                        getWebClient04(t.toString()))
+                .subscribe(response -> log.info("body : {}" ,response.getBody()));
+        stopWatch.stop();
+        log.info("Total Second : {}", stopWatch.getTotalTimeSeconds());
+
+        return Mono.fromSupplier(() -> ResponseEntity.status(HttpStatus.OK).body(responseData));
+    }
+
+    public Mono<ResponseData> getWebClient04(String i) {
+        ResponseData responseData01 = new ResponseData();
+        return webClient.post()
+                .uri("/apiTest")
+                .bodyValue(responseData01.setErrorMsg("http://localhost:18080/nonBlocking - Call nonBlocking webcline04(" + i + ")"))
+                .retrieve()
+                .bodyToMono(ResponseData.class);
+    }
     // 아래 API는 다른 Server에 추가하여 API Test 통신을 받아준다.
     @PostMapping("/apiTest")
+    // 평소 Server 구현 시 ResponseEntity에 ResponseData라는 객체를 담아 처리하기에 이를 그대로 가져왔다.
     public ResponseEntity<ResponseData> apiTest(@RequestBody HashMap<String, Object> paramMap) throws InterruptedException {
         log.trace("Call apiTest.");
 
+        // ResponseServer에서는 전달받은 Data에서 errorMsg를 추출해서 로그로 뿌려주면서 호출 Server를 확인해주고
         String errorMsg = paramMap.get("errorMsg").toString();
         log.info("errorMsg : {}", errorMsg);
+
+        // 응답으로 전달할 메시지를 저장한뒤
         responseData.setErrorMsg("http://localhost:8080/apiTest - Call apiTest.");
 
+        // 3초의 간격을 주었다.
         Thread.sleep(3000);
         return ResponseEntity.status(HttpStatus.OK).body(responseData);
     }
